66c4eabf423e86da3bcfb2eac01cad4748dfd772
Ashley Almonte <ashleyalmonte14@gmail.com>
2019-05-15 09:22:29 -0400

    heree

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-function myFunction () {
 
+let div1 = document.getElementById("div1")
+window.addEventListener("scroll", function(){
+  console.log(window.scrollY)
+  if(window.scrollY > 0) {
+  div1.classList.remove("hidden")
+  div1.classList.add("show")
 }
+})
